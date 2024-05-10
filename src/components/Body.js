9a1e3ee33c41eb1ba7f97d54957687e0a4c0f9e9
Shobhit <shobhitgoyalg@gmail.com>
2024-05-10 18:13:43 +0530

    added filter functionality using useState hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,35 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
-    return (
-      <div className="body">
-        <div className="search">Search</div>
-        <div className="res-container">
-          {resList.map((restaurant) => {
-            return (
-              <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+  //State Variable
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          type="button"
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating >= 4.0
             );
-          })}
-        </div>
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
       </div>
-    );
-  };
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => {
+          return (
+            <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-  export default Body;
+export default Body;
